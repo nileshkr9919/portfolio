7fce791ae79e8136836243b5f027ffaea4df21e1
Nilesh Kumar <68392556+nileshkr9919@users.noreply.github.com>
2026-01-13 19:47:03 +0530

    Update HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
       <h1 className="text-4xl font-bold mb-4 tracking-wide text-center">Nilesh Kumar</h1>
       <h2 className="text-xl mb-4 tracking-tight text-center">Software Engineer • JavaScript Specialist</h2>
       <p className="max-w-2xl text-center mb-8 text-slate-300">
-        I design systems with intention — scalable in architecture, resilient in design, and elegant in developer and user experience. With 3+ years of hands-on experience across backend, frontend, and cloud, I don&apos;t just write code — I craft solutions that last, scale, and evolve.
+        I design systems with intention — scalable in architecture, resilient in design, and elegant in developer and user experience. With 4+ years of hands-on experience across backend, frontend, and cloud, I don&apos;t just write code — I craft solutions that last, scale, and evolve.
       </p>
     </section>
   );
